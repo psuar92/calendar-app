bd9c8570ab0d8de232357c6217cc15b524659021
Pedro <psuar92@gmail.com>
2024-10-19 18:08:48 -0500

    add authSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { calendarSlice, uiSlice } from "./";
+import { authSlice, calendarSlice, uiSlice } from "./";
 
 export const store = configureStore({
     reducer: {
+        auth:     authSlice.reducer,
         calendar: calendarSlice.reducer,
-        ui: uiSlice.reducer,
+        ui:       uiSlice.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
