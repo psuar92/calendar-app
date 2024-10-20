98f9a4488673a5c5b9b9610b35070ef547248c08
Pedro <psuar92@gmail.com>
2024-10-19 21:34:45 -0500

    update Navbar username and logout btn

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,15 +1,22 @@
+import { translations } from '../../helpers';
+import { useAuthStore } from '../../hooks';
+
 export const Navbar = ({changeLang, lang}) => {
 
     const onChangeLang = () => {
         changeLang(lang === 'enUS' ? 'es' : 'enUS');
     }
+
+    const locale = translations[lang];
+
+    const { startLogout, user } = useAuthStore();
     
     return (
         <div className='navbar navbar-dark bg-dark mb-4 px-4'>
             <span className='navbar-brand'>
                 <i className='fas fa-calendar-alt'></i>
                 &nbsp;
-                    Pedro
+                    {user.name}
             </span>
 
             <button className='btn btn-outline-info' onClick={onChangeLang}>
@@ -18,10 +25,10 @@ export const Navbar = ({changeLang, lang}) => {
                 EN/ES
             </button>
 
-            <button className='btn btn-outline-danger'>
+            <button className='btn btn-outline-danger' onClick={startLogout}>
                 <i className='fas fa-sign-out-alt'></i>
                 &nbsp;
-                <span>Logout</span>
+                <span>{locale.logout}</span>
             </button>
         </div>
     );
